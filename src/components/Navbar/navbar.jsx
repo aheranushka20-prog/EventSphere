240f02541651ab94bfd71ec0be7932a9eba5be30
Anushka Aher <anushkaaher20@gmail.com>
2026-09-03 13:53:26 +0530

    Fix Navbar CSS import

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import "./Navbar.css";
+import "./navbar.css";
 
 function Navbar() {
   const [darkMode, setDarkMode] = useState(() => {
